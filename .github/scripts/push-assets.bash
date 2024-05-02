ff7a6d369693bce89297937bfd538701007a8c8d
H1rono <hronok66@gmail.com>
2024-05-02 15:29:46 +0900

    :wrench: Extract script

---
+++ .github/scripts/push-assets.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -uex
+set -o pipefail
+
+git config user.name 'GitHub Action'
+git config user.email 'action@github.com'
+git add assets
+# for debug
+git status
+git diff --quiet assets || {
+	git commit -m 'Update metrics [skip ci]'
+	git push
+}
